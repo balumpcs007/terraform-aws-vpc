ffc7b23360d07b7a6af57809039a4b3214394439
balu <balu@gmail.com>
2026-05-23 08:25:55 +0530

    variable

--- peering.tf
+++ peering.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_peering_connection" "default" {
-count = var.is_peering_requied ? 1 : 0
+count = var.is_peering_required ? 1 : 0
   peer_vpc_id   = data.aws_vpc.default.id # accepter Default vpc id
   vpc_id        = aws_vpc.main.id
 
